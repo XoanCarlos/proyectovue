c69a995e5086db38096c3c223762250fefb1d75b
XoanCarlos <jcarlosperezgonzalez@gmail.com>
2022-12-19 21:55:54 +0100

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="app" class="container">
+    <div class="row">
+      <div class="col-md-12">
+        <h1>CLIENTES</h1>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-md-12">
+        <TablaClientes />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import TablaClientes from "@/components/TablaClientes.vue";
+  export default {
+    name: 'app',
+    components:{
+      TablaClientes,
+    },
+  }
+</script>
+
+<style>
+button{
+  background: #008953;
+  border: 1px solid #008953;
+}
+</style>
---
+++ src/components/TablaClientes.vue
@@ -0,0 +1,40 @@
+<template>
+  <div id ='tabla-clientes'>
+    <table class = 'table'>
+      <thead>
+      <tr>
+        <th>NOMBRE</th>
+        <th>APELLIDOS</th>
+        <th>Email</th>
+      </tr>
+      </thead>
+      <tbody>
+      <tr>
+        <td>Lou</td>
+        <td>Reed</td>
+        <td>lreed@email.com</td>
+      </tr>
+      <tr>
+        <td>Patti</td>
+        <td>Smith</td>
+        <td>psmith@email.com</td>
+      </tr>
+      <tr>
+        <td>Janis</td>
+        <td>Joplin</td>
+        <td>jjoplin@email.com</td>
+      </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "tabla-clientes",
+}
+</script>
+
+<style scoped>
+
+</style>
